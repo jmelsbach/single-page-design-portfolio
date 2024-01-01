ba9593ef4698e377a32ac900d9936cb4969712aa
Johannes Melsbach <github@melsbach.org>
2024-01-01 03:08:17 +0100

    Add IntroductionSection.css and update IntroductionSection.jsx

--- src/components/IntroductionSection.jsx
+++ src/components/IntroductionSection.jsx
@@ -1,5 +1,8 @@
+import './IntroductionSection.css';
 
 export default function IntroductionSection() {
-    return <section></section>;
-
+    return <section>
+        <h1>Design solutions made easy</h1>
+        <p>With over ten years of experience in various design disciplines, I’m your one-stop shop for your design needs.</p>
+    </section>;
 }
