7981b4cec57f7e44bd0cf03b7224dda8f1f28370
Johannes Melsbach <github@melsbach.org>
2024-01-01 02:48:50 +0100

    Update Button and Header styles for responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import logo from '../assets/images/logo.svg';
 import Button from './Button';
 export default function Header(){
     return <header>
-        <img src={logo} alt="logo" />
-        <Button color="black-bg">Free Consultation</Button>
+        <div className='header-container'>
+            <img className="logo" src={logo} alt="logo" />
+            <Button color="black-bg">Free Consultation</Button>
+        </div>
+        
     </header>
 }
